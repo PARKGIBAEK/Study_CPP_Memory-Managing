bd1bdf9e53f8c8d082c87725ceada10d94cc945f
PARKGIBAEK <tkdring@naver.com>
2021-11-17 15:13:46 +0900

    no message

--- MyStudyMaterials/CppServer/Integrated_Version/ServerCore/Allocator.cpp
+++ MyStudyMaterials/CppServer/Integrated_Version/ServerCore/Allocator.cpp
@@ -38,8 +38,11 @@ void* StompAllocator::AllocateMemory(int32 size)
 	- MEM_RESERVE : 메모리 할당 예약(페이징 파일에 실제 물리적 저장공간을 할당하지 않고, 프로세스의 가상 주소 공간 범위를 예약)
 	 
 	- MEM_COMMIT : 예약된 메모리 페이지에 메모리 청구서(할당한 메모리의 크기와 페이징 파일의 크기)를 할당. 가상 주소에 실제로 엑세스하지 않는 한 실제 물리적 페이지는 할당되지 않는다.
-	페이지를 한번 예약하고 커밋하려면 MEM_COMMIT | MEM_RESERVE 옵션을 사용해야한다.
-	 또한 메모리 내용이 0으로 초기화 됨을 보장한다.
+	
+	※ 페이지를 예약하자 마자 사용할 것이라면?
+	  MEM_COMMIT | MEM_RESERVE 옵션을 사용해야한다.
+	  즉, 할당과 동시에 사용할 경우를 말한다.
+	  ( 메모리 내용이 0으로 초기화 됨을 보장한다 )
 
 	- PAGE_READWRITE : 읽기/쓰기 접근 허용, 데이터 실행방지 옵션이 활성화 된 경우 커밋된 영역에서 코드를 실행할 경우 액세스 위반 발생*/
 	void* baseAddress = ::VirtualAlloc(NULL, pageCount * PAGE_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
